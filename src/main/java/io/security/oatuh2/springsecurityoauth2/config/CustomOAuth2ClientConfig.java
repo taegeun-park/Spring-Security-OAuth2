bfbad58c8a5e48e91b2a24862d8bca027c0ee0c1
taegeun <taegeun0525@gmail.com>
2023-08-12 14:46:02 +0900

    feat : CustomOAuth2ClientConfig 작성

---
+++ src/main/java/io/security/oatuh2/springsecurityoauth2/config/CustomOAuth2ClientConfig.java
@@ -0,0 +1,19 @@
+package io.security.oatuh2.springsecurityoauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class CustomOAuth2ClientConfig {
+
+    @Bean
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client();
+        return http.build();
+    }
+}
