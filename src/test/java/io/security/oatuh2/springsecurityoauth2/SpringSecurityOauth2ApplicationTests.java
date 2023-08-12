a434fd7bc2b385f7caf7212d3c0b4f8514d13063
taegeun <taegeun0525@gmail.com>
2023-08-12 13:55:36 +0900

    first commit

---
+++ src/test/java/io/security/oatuh2/springsecurityoauth2/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oatuh2.springsecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
